cf209a4f8e626ca114e6295176008436e2e012ce
maxdk9 <maxdk9@gmail.com>
2019-06-12 06:49:05 +0300

    dice2

---
+++ Assets/Dice.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+	private bool landed = false;
+	private bool thrown = false;
+	private Rigidbody rb;
+	private Vector3 initPosition;
+	public int DiceValue;
+	public DiceSide []  Sides;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		initPosition = transform.position;
+		rb.useGravity = false;
+		
+		
+
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			RollDice();
+		}
+
+		if (rb.IsSleeping() && thrown && !landed)
+		{
+			landed = true;
+			rb.useGravity = false;
+			SideValueCheck();
+		}
+		else if(rb.IsSleeping()&&landed&&this.DiceValue==0)
+		{
+			RollAgain();
+		}
+		
+	}
+
+	private void RollAgain()
+	{
+		Reset();
+		RollDice();
+	}
+
+	private void RollDice()
+	{
+
+		if (thrown && landed)
+		{
+			Reset();
+		}
+		if (thrown||landed)
+		{
+			return;
+		}
+
+		thrown = true;
+		rb.useGravity = true;
+		rb.AddTorque(Random.Range(0,500),Random.Range(0,500),Random.Range(0,500));
+	}
+
+	private void Reset()
+	{
+		transform.position = initPosition;
+		thrown = false;
+		landed = false;
+		rb.useGravity = false;
+
+	}
+
+	void SideValueCheck()
+	{
+		this.DiceValue = 0;
+		foreach (DiceSide side in Sides)
+		{
+			if (side.OnGround)
+			{
+				this.DiceValue = side.Value;
+				Debug.Log("Dice rolled result is "+this.DiceValue);
+			}
+		}
+		
+	}
+}
---
+++ Assets/DiceSide.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceSide : MonoBehaviour
+{
+	public bool OnGround { get; private set; }
+
+	public int Value;
+
+	private void OnTriggerStay(Collider collider)
+	{
+		if (collider.tag == "Ground")
+		{
+			OnGround = true;
+		 }
+	}
+
+	private void OnTriggerExit(Collider collider)
+	{
+		if (collider.tag == "Ground")
+		{
+			OnGround = false;
+		}
+	}
+}
